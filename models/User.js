7ca3425febbff2be3d9ebbef9cb5de2c71bb4c37
Daniel Zamanillo <dzamanillok@gmail.com>
2021-10-09 09:57:34 -0500

    user model created. need to come back to check that thoughts and friends are working

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+	{
+		username: {
+			type: String,
+			unique: true,
+			required: "Please enter username.",
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: "Please enter email.",
+			unique: true,
+			match: [/.+\@.+\..+/],
+		},
+		thoughts: {
+			type: Schema.Types.ObjectId,
+			ref: "Thought",
+		},
+		friends: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+		},
+	},
+	{
+		toJSON: {
+			virtuals: true,
+		},
+		id: false,
+	}
+);
+
+UserSchema.virtual("friendCount").get(function () {
+	return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
