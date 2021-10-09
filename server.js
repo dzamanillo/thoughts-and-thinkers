67864275bcbdae6ea7dce9a174578395cfc6cd80
Daniel Zamanillo <dzamanillok@gmail.com>
2021-10-09 09:40:58 -0500

    basic server set and linked to mongodb

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost/thoughts-and-thinkers",
+	{
+		useFindAndModify: false,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
