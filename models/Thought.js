70523a552d5f3078b8807be9f8ba9a93aac12c48
Daniel Zamanillo <dzamanillok@gmail.com>
2021-10-09 10:32:54 -0500

    thought and reaction models built. need to check them once routes are written

---
+++ models/Thought.js
@@ -0,0 +1,66 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema(
+	{
+		reactionId: {
+			type: Schema.Types.ObjectId,
+			default: () => new Types.ObjectId(),
+		},
+		reactionBody: {
+			type: String,
+			required: "Please enter reaction.",
+			maxLength: [280],
+		},
+		username: {
+			type: String,
+			required: "Please enter username.",
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdAtVal) => dateFormat(createdAtVal),
+		},
+	},
+	{
+		toJSON: {
+			getters: true,
+		},
+	}
+);
+
+const ThoughtSchema = new Schema(
+	{
+		thoughtText: {
+			type: String,
+			required: "Please enter thought.",
+			minLength: [1],
+			maxLength: [280],
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdAtVal) => dateFormat(createdAtVal),
+		},
+		username: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+		},
+		reactions: [reactionSchema],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true,
+		},
+		id: false,
+	}
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+	return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
